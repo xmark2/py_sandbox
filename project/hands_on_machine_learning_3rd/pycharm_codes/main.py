6f29fa361664e528d8589a4a1090a65633a2470c
xmark2 <xmark2@gmail.com>
2024-12-27 15:42:12 +0100

    ML prepare_data options main.py

--- project/hands_on_machine_learning_3rd/pycharm_codes/main.py
+++ project/hands_on_machine_learning_3rd/pycharm_codes/main.py
@@ -5,14 +5,65 @@ import pandas as pd
 import tarfile
 from pathlib import Path
 import urllib.request
+from util import split
 import matplotlib.pyplot as plt
-from sklearn.model_selection import train_test_split
+# from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler, OneHotEncoder
 from sklearn.impute import SimpleImputer
 from sklearn.pipeline import Pipeline
 from sklearn.compose import ColumnTransformer
 from sklearn.linear_model import LinearRegression
 from sklearn.metrics import mean_squared_error
+
+from sklearn.metrics.pairwise import rbf_kernel
+from sklearn.base import BaseEstimator, TransformerMixin
+from sklearn.utils.validation import check_array, check_is_fitted
+
+
+class StandardScalerClone(BaseEstimator, TransformerMixin):
+    def __init__(self, with_mean=True):  # no *args or **kwargs!
+        self.with_mean = with_mean
+
+    def fit(self, X, y=None):  # y is required even though we don't use it
+        X = check_array(X)  # checks that X is an array with finite float values
+        self.mean_ = X.mean(axis=0)
+        self.scale_ = X.std(axis=0)
+        self.n_features_in_ = X.shape[1]  # every estimator stores this in fit()
+        return self  # always return self!
+
+    def transform(self, X):
+        check_is_fitted(self)  # looks for learned attributes (with trailing _)
+        X = check_array(X)
+        assert self.n_features_in_ == X.shape[1]
+        if self.with_mean:
+            X = X - self.mean_
+        return X / self.scale_
+
+
+# In[96]:
+
+
+from sklearn.cluster import KMeans
+
+
+class ClusterSimilarity(BaseEstimator, TransformerMixin):
+    def __init__(self, n_clusters=10, gamma=1.0, random_state=None):
+        self.n_clusters = n_clusters
+        self.gamma = gamma
+        self.random_state = random_state
+
+    def fit(self, X, y=None, sample_weight=None):
+        self.kmeans_ = KMeans(self.n_clusters, n_init=10,
+                              random_state=self.random_state)
+        self.kmeans_.fit(X, sample_weight=sample_weight)
+        return self  # always return self!
+
+    def transform(self, X):
+        return rbf_kernel(X, self.kmeans_.cluster_centers_, gamma=self.gamma)
+
+    def get_feature_names_out(self, names=None):
+        return [f"Cluster {i} similarity" for i in range(self.n_clusters)]
+
 
 
 ## Download the Data
@@ -27,7 +78,7 @@ def download_housing_data():
     return pd.read_csv(Path("datasets/housing/housing.csv"))
 
 
-def explore_data(data):
+def explore_data(data, inf):
     """
     Display the first few rows of the dataset and plot histograms of all features.
 
@@ -41,26 +92,31 @@ def explore_data(data):
         return
 
     pd.options.plotting.backend = "matplotlib"
-    print('-' * 10)
-    print('SHAPE')
-    print(data.shape)
+    # print('-' * 10)
+    if str(inf).lower().startswith('shape'):
+        # print('SHAPE')
+        print(data.shape)
 
-    print('-'*10)
-    print('HEAD')
-    print(data.head())
+    # print('-'*10)
+    if str(inf).lower().startswith('head'):
+        # print('HEAD')
+        print(data.head())
 
-    print('-' * 10)
-    print('INFO')
-    print(data.info())
+    # print('-' * 10)
+    # print('INFO')
+    if str(inf).lower().startswith('info'):
+        print(data.info())
 
-    print('-' * 10)
-    print('Value_Counts')
-    for col in data.columns:
-        print(data[col].value_counts())
+    # print('-' * 10)
+    # print('Value_Counts')
+    if str(inf).lower().startswith('val'):
+        for col in data.columns:
+            print(data[col].value_counts())
 
-    print('-' * 10)
-    print('Describe')
-    print(data.describe())
+    # print('-' * 10)
+    # print('Describe')
+    if str(inf).lower().startswith('desc'):
+        print(data.describe())
 
     # data.hist(bins=50, figsize=(20, 15))
     # plt.show()
@@ -76,7 +132,8 @@ def split_data(data):
     Returns:
     tuple: A tuple containing the training set and the test set as pandas DataFrames.
     """
-    train_set, test_set = train_test_split(data, test_size=0.2, random_state=42)
+    # train_set, test_set = train_test_split(data, test_size=0.2, random_state=42)
+    train_set, test_set = split.split_train_test_split_random(data)
     return train_set, test_set
 
 
@@ -90,23 +147,96 @@ def prepare_data(train_set):
     Returns:
     tuple: A tuple containing the prepared features and the labels as NumPy arrays.
     """
+    # housing_num = housing.select_dtypes(include=[np.number])
+
     housing = train_set.drop("median_house_value", axis=1)
     housing_labels = train_set["median_house_value"].copy()
 
+    # num_pipeline Option1
     num_pipeline = Pipeline([
         ('imputer', SimpleImputer(strategy="median")),
         ('std_scaler', StandardScaler()),
     ])
 
-    num_attribs = list(housing.drop("ocean_proximity", axis=1))
+    from sklearn.pipeline import make_pipeline
+
+    # num_pipeline Option2
+    num_pipeline = make_pipeline(SimpleImputer(strategy="median"), StandardScaler())
+
+    cat_pipeline = make_pipeline(
+        SimpleImputer(strategy="most_frequent"),
+        OneHotEncoder(handle_unknown="ignore"))
+
+    # preprocessing Option1
+    num_attribs = ["longitude", "latitude", "housing_median_age", "total_rooms",
+                   "total_bedrooms", "population", "households", "median_income"]
     cat_attribs = ["ocean_proximity"]
 
-    full_pipeline = ColumnTransformer([
+    preprocessing = ColumnTransformer([
         ("num", num_pipeline, num_attribs),
-        ("cat", OneHotEncoder(), cat_attribs),
+        ("cat", cat_pipeline, cat_attribs),
     ])
 
-    housing_prepared = full_pipeline.fit_transform(housing)
+    from sklearn.compose import make_column_selector, make_column_transformer
+
+    # preprocessing Option2
+    preprocessing = make_column_transformer(
+        (num_pipeline, make_column_selector(dtype_include=np.number)),
+        (cat_pipeline, make_column_selector(dtype_include=object)),
+    )
+
+    housing_prepared = preprocessing.fit_transform(housing)
+
+    # extra code – shows that we can get a DataFrame out if we want
+    housing_prepared_fr = pd.DataFrame(
+        housing_prepared,
+        columns=preprocessing.get_feature_names_out(),
+        index=housing.index)
+    print(housing_prepared_fr.head(2))
+
+    from sklearn.preprocessing import FunctionTransformer
+
+    def column_ratio(X):
+        return X[:, [0]] / X[:, [1]]
+
+    def ratio_name(function_transformer, feature_names_in):
+        return ["ratio"]  # feature names out
+
+    def ratio_pipeline():
+        return make_pipeline(
+            SimpleImputer(strategy="median"),
+            FunctionTransformer(column_ratio, feature_names_out=ratio_name),
+            StandardScaler())
+
+    log_pipeline = make_pipeline(
+        SimpleImputer(strategy="median"),
+        FunctionTransformer(np.log, feature_names_out="one-to-one"),
+        StandardScaler())
+    cluster_simil = ClusterSimilarity(n_clusters=10, gamma=1., random_state=42)
+    default_num_pipeline = make_pipeline(SimpleImputer(strategy="median"),
+                                         StandardScaler())
+
+    preprocessing = ColumnTransformer([
+        ("bedrooms", ratio_pipeline(), ["total_bedrooms", "total_rooms"]),
+        ("rooms_per_house", ratio_pipeline(), ["total_rooms", "households"]),
+        ("people_per_house", ratio_pipeline(), ["population", "households"]),
+        ("log", log_pipeline, ["total_bedrooms", "total_rooms", "population",
+                               "households", "median_income"]),
+        ("geo", cluster_simil, ["latitude", "longitude"]),
+        # ("num", num_pipeline, make_column_selector(dtype_include=np.number)),
+        ("cat", cat_pipeline, make_column_selector(dtype_include=object)),
+    ],
+        remainder=default_num_pipeline)  # one column remaining: housing_median_age
+
+    housing_prepared = preprocessing.fit_transform(housing)
+
+    # extra code – shows that we can get a DataFrame out if we want
+    housing_prepared_fr = pd.DataFrame(
+        housing_prepared,
+        columns=preprocessing.get_feature_names_out(),
+        index=housing.index)
+    print(housing_prepared_fr.head(2))
+
     return housing_prepared, housing_labels
 
 
@@ -147,12 +277,14 @@ def evaluate_model(model, housing_prepared, housing_labels):
 def execute():
     housing_data = download_housing_data()
 
+    # print(housing_data.select_dtypes(include=[np.number]).columns)
+    # text_data = housing_data.select_dtypes(include=['object'])
+    # print(np.number)
 
+    explore_data(housing_data, inf='info')
 
-    explore_data(housing_data)
-
-    # train_set, test_set = split_data(housing_data)
-    # housing_prepared, housing_labels = prepare_data(train_set)
+    train_set, test_set = split_data(housing_data)
+    housing_prepared, housing_labels = prepare_data(train_set)
     #
     # model = train_model(housing_prepared, housing_labels)
     #
